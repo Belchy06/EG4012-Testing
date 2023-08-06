36d35826e774656e4e8385f2ed541e469c39b8c8
William Belcher <william.belcher@hotmail.com>
2023-08-07 08:54:15 +1000

    Initial commit

---
+++ server.cpp
@@ -0,0 +1,16 @@
+#ifndef WIN32_MEAN_AND_LEAN
+	#define WIN32_MEAN_AND_LEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+	return 0;
+}
